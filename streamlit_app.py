acd3ed98ee31f27608d86a1560da8f91b8ab04c7
dineshvure <68220308+dineshvure@users.noreply.github.com>
2025-01-10 17:15:16 -0500

    Update streamlit_app.py
    added # from line 10 to 13

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,8 @@ streamlit.write('Breakfast Menu')
 
 #st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 #st.write(
-    """Choose the fruits you want in your custom Smoothie!
-    """)
+#    """Choose the fruits you want in your custom Smoothie!
+#   """)
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
